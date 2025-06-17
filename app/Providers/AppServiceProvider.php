f49d2f376d2dcb436f5c8b4b37329d9576596ca8
nrlzaty <nurulizzati1377@gmail.com>
2025-06-17 12:18:32 +0800

    commit new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'deployment') {
             URL::forceScheme('https');
         }
     }
