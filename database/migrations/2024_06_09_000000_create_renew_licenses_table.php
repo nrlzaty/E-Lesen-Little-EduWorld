3650ced5dfc1d1b63fab3df0097ba9dab7d5a365
nrlzaty <nurulizzati1377@gmail.com>
2025-06-19 15:21:47 +0800

    commit renew licenses

--- database/migrations/2024_06_09_000000_create_renew_licenses_table.php
+++ database/migrations/2024_06_09_000000_create_renew_licenses_table.php
@@ -8,19 +8,18 @@ class CreateRenewLicensesTable extends Migration
 {
    public function up()
 {
-    if (!Schema::hasTable('renew_licenses')) {
-        Schema::create('renew_licenses', function (Blueprint $table) {
+    Schema::create('renew_licenses', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('applicant_id');
             $table->unsignedBigInteger('previous_applicant_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            $table->foreign('applicant_id')->references('id')->on('applicants');
-            $table->foreign('previous_applicant_id')->references('id')->on('applicants');
+            // $table->foreign('applicant_id')->references('id')->on('applicants');
+            // $table->foreign('previous_applicant_id')->references('id')->on('applicants');
             $table->foreign('user_id')->references('id')->on('users');
         });
-    }
+    
 }
 
     public function down()
